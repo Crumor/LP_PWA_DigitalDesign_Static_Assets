3631e004e25a81d9706c0d02aea4cc76e33d1ebd
José Luis Casstro García <jlcastrog@liverpool.com.mx>
2019-06-20 10:07:41 -0500

    Assets Muebles
    BLP Muebles assets

---
+++ assetsB/digital/landing/muebles/js/main.js
@@ -0,0 +1,193 @@
+
+
+/*PRINCIPAL*/
+
+$('.principal-muebles').mouseenter(function()
+{
+    $('.principal-muebles-circulo h5').css("font-size", "28px");
+    $('.principal-muebles-circulo p').css("font-size", "20pxvw").css("opacity", "0").css("padding-top", "20%");
+    $('.linea-lb').css("opacity", "0");
+});
+
+
+$('.principal-muebles').mouseleave(function()
+{
+    $('.principal-muebles-circulo h5').css("font-size", "20px");
+    $('.principal-muebles-circulo p').css("font-size", "28px").css("opacity", "1").css("padding-top", "30%");
+    $('.linea-lb').css("opacity", "1");
+
+});
+
+
+
+/*MESAS*/
+
+/*$('.principal-mesas').mouseenter(function()
+{
+    $('.principal-mesas-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-mesas-texto-2').css("right", "0").css("opacity", "1");
+
+});
+
+
+$('.principal-mesas').mouseleave(function()
+{
+    $('.principal-mesas-texto').css("right", "0").css("opacity", "1");
+    $('.principal-mesas-texto-2').css("right", "30%").css("opacity", "0");
+
+});*/
+
+
+
+/*CAMAS*/
+
+$('.principal-camas').mouseenter(function()
+{
+    $('.principal-camas-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-camas-texto-2').css("right", "0").css("opacity", "1");
+
+});
+
+
+$('.principal-camas').mouseleave(function()
+{
+    $('.principal-camas-texto').css("right", "0").css("opacity", "1");
+    $('.principal-camas-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+/*SECADORAS*/
+
+$('.principal-sillas').mouseenter(function()
+{
+    $('.principal-sillas-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-sillas-texto-2').css("right", "0").css("opacity", "1");
+
+});
+
+
+$('.principal-sillas').mouseleave(function()
+{
+    $('.principal-sillas-texto').css("right", "0").css("opacity", "1");
+    $('.principal-sillas-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+/*ESPEJOS*/
+
+$('.principal-espejos').mouseenter(function()
+{
+    $('.principal-espejos-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-espejos-texto-2').css("right", "15%").css("opacity", "1");
+
+});
+
+
+$('.principal-espejos').mouseleave(function()
+{
+    $('.principal-espejos-texto').css("right", "0").css("opacity", "1");
+    $('.principal-espejos-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+
+/*COJINES*/
+
+$('.principal-cojines').mouseenter(function()
+{
+    $('.principal-cojines-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-cojines-texto-2').css("right", "15%").css("opacity", "1");
+
+});
+
+
+$('.principal-cojines').mouseleave(function()
+{
+    $('.principal-cojines-texto').css("right", "0").css("opacity", "1");
+    $('.principal-cojines-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+
+/*CUADROS*/
+
+$('.principal-cuadros').mouseenter(function()
+{
+    $('.principal-cuadros-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-cuadros-texto-2').css("right", "15%").css("opacity", "1");
+
+});
+
+
+$('.principal-cuadros').mouseleave(function()
+{
+    $('.principal-cuadros-texto').css("right", "0").css("opacity", "1");
+    $('.principal-cuadros-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+
+
+
+/*IDEAS*/
+
+$('.principal-ideas').mouseenter(function()
+{
+    $('.principal-ideas-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-ideas-texto-2').css("right", "0").css("opacity", "1");
+
+});
+
+
+$('.principal-ideas').mouseleave(function()
+{
+    $('.principal-ideas-texto').css("right", "0").css("opacity", "1");
+    $('.principal-ideas-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+
+
+
+/*DEPA*/
+
+$('.principal-depa').mouseenter(function()
+{
+    $('.principal-depa-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-depa-texto-2').css("right", "0").css("opacity", "1");
+
+});
+
+
+$('.principal-depa').mouseleave(function()
+{
+    $('.principal-depa-texto').css("right", "0").css("opacity", "1");
+    $('.principal-depa-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
+
+
+/*JARDÍN*/
+
+$('.principal-jardin').mouseenter(function()
+{
+    $('.principal-jardin-texto').css("right", "30%").css("opacity", "0");
+    $('.principal-jardin-texto-2').css("right", "0").css("opacity", "1");
+
+});
+
+
+$('.principal-jardin').mouseleave(function()
+{
+    $('.principal-jardin-texto').css("right", "0").css("opacity", "1");
+    $('.principal-jardin-texto-2').css("right", "30%").css("opacity", "0");
+
+});
+
