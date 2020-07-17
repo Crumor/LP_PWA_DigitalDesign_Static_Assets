cd160c416d27fa10c9352d017d21e8c5ee6c49e3
Liverpool Diseño Digital <49457113+LiverpoolDigital@users.noreply.github.com>
2020-07-17 15:00:00 -0500

    adobe analytics google

--- assetsB/assets/digital/analytics/analytics.js
+++ assetsB/assets/digital/analytics/analytics.js
@@ -1,401 +1,100 @@
-
     function myOnloadFunction(pageName,path) {
     	switch(path){
 
     		case '/tienda/home':
-				dataLayer.push({
-                'event': 'impresionCampaign',
-                'ecommerce': {
-                    'promoView': {
-                        'promotions': [// Array of promoFieldObjects.
+        //IMPRESION CAMPAIGN DE GOOGLE HOME LIVERPOOL
+				var objimpression = [// Array of promoFieldObjects.
                             {
            'id':'covid19_010420',
             'name':'Comunicado COVID-19',
            'creative':'h_banner_central',
            'position':'A',
-			 },{
-		   'id':'mujer_220620',
+       },{
+       'id':'mujer_220620',
                 'name':'Mujer hasta 43% de desc',
                 'creative':'h_banner_secun',
                 'position':'1',
-			},{
-			'id':'hombre_220620',
+      },{
+      'id':'hombre_220620',
                 'name':'Hombre hasta 43% de desc',
                 'creative':'h_banner_secun',
                 'position':'2',
-			 },
-			 {
-		 'id':'ninas_y_ninos_220620',
+       },
+       {
+     'id':'ninas_y_ninos_220620',
                 'name':'Niñas y Niños hasta 43% de desc',
                 'creative':'h_banner_secun',
                 'position':'3',
-			 }, {
-		'id':'bebes_220620',
+       }, {
+    'id':'bebes_220620',
                 'name':'Bebes hasta 43% de desc',
                 'creative':'h_banner_secun',
                 'position':'4',
-			 },
-			  {
-		  'id':'zapatos_220620',
+       },
+        {
+      'id':'zapatos_220620',
            'name':'Zapatos hasta 43% de desc',
            'creative':'h_banner_secun',
            'position':'5',
-			 },
-			 {
-		 'id':'muebles_de_jardin_150620',
+       },
+       {
+     'id':'muebles_de_jardin_150620',
        'name':'Muebles de jardín hasta 30% de desc',
        'creative':'h_banner_secun',
        'position':'6',
-			 }, {
-		  'id':'cocina_220620',
+       }, {
+      'id':'cocina_220620',
             'name':'Cocina hasta 25% de desc',
             'creative':'h_banner_secun',
             'position':'7',
-			 },
-			{
-			'id':'aires_acondicionados_220620',
+       },
+      {
+      'id':'aires_acondicionados_220620',
             'name':'Aires Acondicionados hasta 40% de desc',
             'creative':'h_banner_secun',
             'position':'8'
-			 },
+       },
                  {
-			'id':'deportes_220620',
+      'id':'deportes_220620',
        'name':'Deportes hasta 43% de desc',
        'creative':'h_banner_secun',
        'position':'9',
-			 },
+       },
             {
-		  'id':'relojes_220620',
+      'id':'relojes_220620',
             'name':'Relojes hasta 25% de desc',
             'creative':'h_banner_secun',
             'position':'10',
-			 },
-        	 {
-		  'id':'pandora_220620',
+       },
+           {
+      'id':'pandora_220620',
         'name':'Pandora hasta 40% de desc',
         'creative':'h_banner_secun',
         'position':'11',
                  }
-                        ]
-                    }
-                }
-            });
+                        ];
+
+var objAdobe ={
+    eventName: 'Impression Campaign',
+    eventAction: 'customImpressionCampaign',
+    attributes:{
+        'promotions':objimpression
+    }
+  }
+var objGoogle = {
+    'event': 'impresionCampaign',
+    'ecommerce': {
+          'promoView': {
+                  'promotions': objimpression
+        }
+   }
+ }
+ 
+dataLayer.push(objGoogle);//google
+window.digitalData.event = window.digitalData.event || [];//adobe
+window.digitalData.event.push(objAdobe);
     		break;
-    		case '/tienda/él/cat5020003':
-
-
-dataLayer.push({
-'event': 'impresionCampaign',
-'ecommerce': {
-'promoView': {
-'promotions': [ // Array of promoFieldObjects.
-{
-'id': 'lifestyle_main_180619',
-'name': 'Lifestyle Principal',
-'creative': 'blp_el_lifestyle_main',
-'position': '01'
-},
-{
-'id': 'el_jeans_180619',
-'name': 'El Jeans',
-'creative': 'blp_el_jeans',
-'position': '02'
-},
-{
-'id': 'el_accesorios_180619',
-'name': 'El Accesorios',
-'creative': 'blp_el_accesorios',
-'position': '03'
-},
-{
-'id': 'el_polo_180619',
-'name': 'El Polo',
-'creative': 'blp_el_polo',
-'position': '04'
-},
-{
-'id': 'new_arrivals_130519',
-'name': 'New Arrivals Él',
-'creative': 'blp_el_New_Arrivals',
-'position': '05'
-},
-{
-'id': 'new_arrivals_1_130519',
-'name': 'New Arrivals 1',
-'creative': 'blp_el_New_Arrivals_1',
-'position': '06'
-},
-{
-'id': 'new_arrivals_2_130519',
-'name': 'New Arrivals 2',
-'creative': 'blp_el_New_Arrivals_2',
-'position': '07'
-},
-{
-'id': 'new_arrivals_3_130519',
-'name': 'New Arrivals 3',
-'creative': 'blp_el_New_Arrivals_3',
-'position': '08'
-},
-{
-'id': 'new_arrivals_4_130519',
-'name': 'New Arrivals 4',
-'creative': 'blp_el_New_Arrivals_4',
-'position': '09'
-},
-{
-'id': 'new_arrivals_5_130519',
-'name': 'New Arrivals 5',
-'creative': 'blp_el_New_Arrivals_5',
-'position': '10'
-},
-{
-'id': 'new_arrivals_6_130519',
-'name': 'New Arrivals 6',
-'creative': 'blp_el_New_Arrivals_6',
-'position': '11'
-},
-{
-'id': 'boss_hugo_130519',
-'name': 'Boss Hugo',
-'creative': 'blp_el_boss',
-'position': '12'
-},
-{
-'id': 'street_playeras_130519',
-'name': 'Street Playeras',
-'creative': 'blp_el_street_playeras',
-'position': '13'
-},
-{
-'id': 'street_mochilas_130519',
-'name': 'Street Mochilas',
-'creative': 'blp_el_street_mochilas',
-'position': '14'
-},
-{
-'id': 'street_outerwear_130519',
-'name': 'Street OuterWear',
-'creative': 'blp_el_street_outerwear',
-'position': '15'
-},
-{
-'id': 'marcas_el_hugo_130519',
-'name': 'Marcas Hugo',
-'creative': 'blp_el_marcas_hugo',
-'position': '16'
-},
-{
-'id': 'marcas_el_banana_130519',
-'name': 'Marcas Banana',
-'creative': 'blp_el_marcas_banana',
-'position': '17'
-},
-{
-'id': 'marcas_el_american_130519',
-'name': 'Marcas American',
-'creative': 'blp_el_marcas_american',
-'position': '18'
-},
-{
-'id': 'marcas_el_jcrew_130519',
-'name': 'Marcas JCrew',
-'creative': 'blp_el_marcas_jcrew',
-'position': '19'
-},
-{
-'id': 'marcas_el_psycho_130519',
-'name': 'Marcas Psycho',
-'creative': 'blp_el_marcas_psycho',
-'position': '20'
-},
-{
-'id': 'marcas_el_gap_130519',
-'name': 'Marcas Gap',
-'creative': 'blp_el_marcas_gap',
-'position': '21'
-},
-{
-'id': 'marcas_el_ben_130519',
-'name': 'Marcas Ben',
-'creative': 'blp_el_marcas_ben',
-'position': '22'
-},
-{
-'id': 'marcas_el_ralph_130519',
-'name': 'Marcas Ralph',
-'creative': 'blp_el_marcas_ralph',
-'position': '23'
-}
-]
-}
-}
-});
-
-
-    		break;
-    		case '/tienda/bebés/cat4120003':
-  dataLayer.push({
-'event': 'impresionCampaign',
-'ecommerce': {
-'promoView': {
-'promotions': [ // Array of promoFieldObjects.
-{
-'id': 'regalo_main_040619',
-'name': 'Regalo Compra',
-'creative': 'blp_bebes_regalo',
-'position': '01'
-},
-{
-'id': 'paco_rabanne_040619',
-'name': 'Paco Rabanne',
-'creative': 'blp_belleza_paco_rabanne',
-'position': '02'
-},
-{
-'id': 'bases_maquillaje_040619',
-'name': 'Bases Maquillaje',
-'creative': 'blp_belleza_bases',
-'position': '03'
-},
-{
-'id': 'hair_care_040619',
-'name': 'Hair Care',
-'creative': 'blp_belleza_hair_care',
-'position': '04'
-},
-{
-'id': 'make_up_040619',
-'name': 'Make Up',
-'creative': 'blp_belleza_make_up',
-'position': '05'
-},
-{
-'id': 'perfumes_040619',
-'name': 'Perfumes',
-'creative': 'blp_belleza_perfumes',
-'position': '06'
-},
-{
-'id': 'skin_care_040619',
-'name': 'Skin Care',
-'creative': 'blp_belleza_skin_care',
-'position': '07'
-},
-{
-'id': 'hombres_040619',
-'name': 'Hombres Belleza',
-'creative': 'blp_belleza_hombres',
-'position': '08'
-},
-{
-'id': 'pickbox_040619',
-'name': 'Pickbox Belleza',
-'creative': 'blp_belleza_pickbox',
-'position': '09'
-},
-{
-'id': 'crema_ojos_040619',
-'name': 'Crema ojos',
-'creative': 'blp_belleza_crema_ojos',
-'position': '10'
-},
-{
-'id': 'mascarillas_040619',
-'name': 'Mascarillas',
-'creative': 'blp_belleza_mascarillas',
-'position': '11'
-},
-{
-'id': 'corporales_040619',
-'name': 'Corporales',
-'creative': 'blp_belleza_corporales',
-'position': '12'
-},
-{
-'id': 'faciales_040619',
-'name': 'Faciales',
-'creative': 'blp_belleza_faciales',
-'position': '13'
-},
-{
-'id': 'eros_flame_040619',
-'name': 'Eros Flame',
-'creative': 'blp_belleza_eros_flame',
-'position': '14'
-},
-{
-'id': 'anastasia_040619',
-'name': 'Anastasia',
-'creative': 'blp_belleza_anastasia',
-'position': '15'
-},
-{
-'id': 'dior_040619',
-'name': 'Dior',
-'creative': 'blp_belleza_dior',
-'position': '16'
-},
-{
-'id': 'estee_040619',
-'name': 'Estee',
-'creative': 'blp_belleza_estee',
-'position': '17'
-},
-{
-'id': 'giorgio_040619',
-'name': 'Giorgio',
-'creative': 'blp_belleza_giorgio',
-'position': '18'
-},
-{
-'id': 'guerlain_040619',
-'name': 'Guerlain',
-'creative': 'blp_belleza_guerlain',
-'position': '19'
-},
-{
-'id': 'givenchy_040619',
-'name': 'Givenchy',
-'creative': 'blp_belleza_givenchy',
-'position': '20'
-},
-{
-'id': 'kiehls_040619',
-'name': 'Kiehls',
-'creative': 'blp_belleza_kiehls',
-'position': '21'
-},
-{
-'id': 'lancome_040619',
-'name': 'Lancome',
-'creative': 'blp_belleza_lancome',
-'position': '22'
-},
-{
-'id': 'mac_040619',
-'name': 'Mac',
-'creative': 'blp_belleza_mac',
-'position': '23'
-},
-{
-'id': 'urban_decay_040619',
-'name': 'Urban Decay',
-'creative': 'blp_belleza_urban_decay',
-'position': '24'
-},
-{
-'id': 'bx_040619',
-'name': 'BX',
-'creative': 'blp_belleza_bx',
-'position': '25'
-}
-]
-}
-}
-});
-    		break;
-    	
+    		
     	
     	}
     }
