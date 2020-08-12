03a86834b9ac9b2616d026b82b769b1571769afd
dperezp02 <45075845+dperezp02@users.noreply.github.com>
2020-08-12 18:02:42 -0500

    Create mis-seguros-iframe.js
    Se crea script para obtener los queryParams de seguros

---
+++ assetsB/assets/misSeguros/mis-seguros-iframe.js
@@ -0,0 +1,4 @@
+
+if(window.location.search.toString()){
+        document.getElementById("my_object").src = "https://miseguroqa.liverpool.com.mx/miseguro_dev/mis-polizas" + window.location.search.toString();
+    }
