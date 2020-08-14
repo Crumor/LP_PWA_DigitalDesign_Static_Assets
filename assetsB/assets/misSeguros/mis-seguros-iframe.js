87b5d0e8a241fd9cbdcf9643322d886a97fe53b0
dperezp02 <45075845+dperezp02@users.noreply.github.com>
2020-08-14 09:13:50 -0500

    Update mis-seguros-iframe.js
    Add timeout

--- assetsB/assets/misSeguros/mis-seguros-iframe.js
+++ assetsB/assets/misSeguros/mis-seguros-iframe.js
@@ -1,5 +1,5 @@
-
 if(window.location.search.toString()){
-        console.log('src ', document.getElementById("my_object").src);
-        document.getElementById("my_object").src = "https://miseguroqa.liverpool.com.mx/miseguro_dev/mis-polizas" + window.location.search.toString();
-    }
+  setTimeout(function(){ 
+      document.getElementById("my_object").src = "https://miseguroqa.liverpool.com.mx/miseguro_dev/mis-polizas" + window.location.search.toString();
+    }, 2000);
+  }
